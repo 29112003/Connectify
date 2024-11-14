751a6f882cae73e5903ffd6ad8f80f8ed7bb0703
29112003 <thisiskeshavyadavv@gmail.com>
2024-11-14 17:05:29 +0530

    handling errors and creating asyncError handlers

--- app.js
+++ app.js
@@ -9,4 +9,15 @@ app.use(logger("tiny"));
 // routes
 app.get("/",require("./routes/indexRouters"));
 
+const ErrorHandler = require("./utils/ErrorHandler")
+const {genetatedErrors} = require("./middlerwares/Errors")
+// error handling 
+
+
+app.all("*", (req,res,next)=>{
+    next(new ErrorHandler(`Requested URL Not Found ${req.url}`, 404));
+  })    
+  
+app.use(genetatedErrors)
+
 app.listen(process.env.PORT, console.log(`server is running on port ${process.env.PORT}`));
